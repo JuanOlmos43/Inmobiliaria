b6f371e476e7dbecb4dbdab2faf66f813e359953
Muller Virgi <virgimuller27957@gmail.com>
2026-01-19 21:58:10 -0300

    app modules update: users and auth

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,9 +4,11 @@ import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { PropiedadesModule } from './propiedades/propiedades.module';
 import { UbicacionesModule } from './ubicaciones/ubicaciones.module';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [PrismaModule, PropiedadesModule, UbicacionesModule],
+  imports: [PrismaModule, PropiedadesModule, UbicacionesModule, UsersModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
