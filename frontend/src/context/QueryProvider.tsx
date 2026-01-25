cf01a5f4dd6f950d5a8eec1aadc3a365d4628323
Muller Virgi <virgimuller27957@gmail.com>
2026-01-25 10:19:41 -0300

    dev tools tanstack query

--- frontend/src/context/QueryProvider.tsx
+++ frontend/src/context/QueryProvider.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { useState, ReactNode } from 'react';
 
 export default function QueryProvider({ children }: { children: ReactNode }) {
@@ -17,6 +18,7 @@ export default function QueryProvider({ children }: { children: ReactNode }) {
     return (
         <QueryClientProvider client={queryClient}>
             {children}
+            <ReactQueryDevtools initialIsOpen={false} />
         </QueryClientProvider>
     );
 }
